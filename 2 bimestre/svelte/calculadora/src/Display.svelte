4eda7e54b4b57d5ddbec396360485046a6c8f431
Angeline Rudnik <ange.sofi.rr@gmail.com>
2025-06-08 23:57:38 -0300

    pós-modularizaçao

---
+++ 2 bimestre/svelte/calculadora/src/Display.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let value = "";
+</script>
+
+<input id="console" type="text" readonly bind:value={value} />
+<style>
+  input {
+    width: 25vw;
+    height: 8vh;
+    color: rgb(71, 37, 102);
+    outline: 0;
+    border: 3px solid rgb(71, 37, 102);
+    border-radius: 5px;
+    font-size: 25px;
+    text-align: right;
+    padding: 0 10px;
+    margin-bottom: 10px;
+  }
+</style>
---
+++ 2 bimestre/svelte/calculadora/src/Keyboard.svelte
@@ -0,0 +1,51 @@
+<script>
+  export let onDigit;
+  export let onOperation;
+  export let onResolve;
+  export let onClear;
+</script>
+
+<div class="keyboard">
+  <div class="first-row">
+    <button on:click={() => onDigit("1")}>1</button>
+    <button on:click={() => onDigit("2")}>2</button>
+    <button on:click={() => onDigit("3")}>3</button>
+    <button on:click={() => onOperation("sum")}>+</button>
+  </div>
+  <div class="second-row">
+    <button on:click={() => onDigit("4")}>4</button>
+    <button on:click={() => onDigit("5")}>5</button>
+    <button on:click={() => onDigit("6")}>6</button>
+    <button on:click={() => onOperation("subtract")}>-</button>
+  </div>
+  <div class="third-row">
+    <button on:click={() => onDigit("7")}>7</button>
+    <button on:click={() => onDigit("8")}>8</button>
+    <button on:click={() => onDigit("9")}>9</button>
+    <button on:click={() => onOperation("multiply")}>x</button>
+  </div>
+  <div class="last-row">
+    <button on:click={onResolve}>=</button>
+    <button on:click={() => onDigit("0")}>0</button>
+    <button on:click={onClear}>C</button>
+    <button on:click={() => onOperation("divide")}>÷</button>
+  </div>
+</div>
+
+<style>
+  button {
+    background-color: rgb(71, 37, 102);
+    margin: 5px;
+    width: 6vw;
+    height: 7vh;
+    border: 0;
+    border-radius: 11px;
+    cursor: pointer;
+    color: white;
+    font-size: 25px;
+  }
+  button:hover {
+    background-color: rgb(40, 21, 58);
+  }
+
+</style>
